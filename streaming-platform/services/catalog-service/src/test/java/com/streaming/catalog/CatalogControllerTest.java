e42835c248ca5239e22d3eb1c8a9af97a85fdfef
Super User <root@devops-vm-1.asia-south1-c.c.devops-1-502311.internal>
2026-08-04 19:08:03 +0530

    fix: import com.streaming.catalog.CatalogController.Title; in catalogcontroller.java

--- streaming-platform/services/catalog-service/src/test/java/com/streaming/catalog/CatalogControllerTest.java
+++ streaming-platform/services/catalog-service/src/test/java/com/streaming/catalog/CatalogControllerTest.java
@@ -1,6 +1,7 @@
 package com.streaming.catalog;
 
 import org.junit.jupiter.api.Test;
+import com.streaming.catalog.CatalogController.Title;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
