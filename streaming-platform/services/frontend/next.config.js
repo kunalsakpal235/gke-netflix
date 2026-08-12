61f538470c55b4d6c968158e79e4cac1757403e8
Super User <root@devops-vm-1.asia-south1-c.c.devops-1-502311.internal>
2026-08-12 20:37:59 +0530

    fix: updated frontend service code next-config.js

--- streaming-platform/services/frontend/next.config.js
+++ streaming-platform/services/frontend/next.config.js
@@ -1,1 +1,12 @@
-module.exports = { output: 'standalone' };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  webpack: (config, { dev }) => {
+    if (config.cache && !dev) {
+      config.cache = Object.freeze({ type: 'memory' })
+    }
+    return config
+  },
+}
+
+module.exports = nextConfig
